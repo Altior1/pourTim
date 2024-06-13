11fb3d4077aa6e52083d44927a8ee6963bc513f2
Altior1 <thomas.rud@outlook.fr>
2024-06-13 14:35:19 +0200

    création des classe #1 #2  ajout de la bdd #4

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlite3
+import mesclasses
+
+if __name__ =="__main__" :
+    start()
+
+
+def start():
+    
---
+++ mesclasses.py
@@ -0,0 +1,20 @@
+class Eleve:
+    def __init__(self) -> None:
+        self.classe=""
+        self.nom=""
+        self.prenom=""
+    
+#un elève pour initialisé
+Michel=Eleve()
+
+class Carte:
+    def __init__(self) -> None:
+        self.numéro=""
+        self.proprio=Michel
+    def attribution(élève):
+        self.proprio=élève
+
+class Borne:
+    def __init__(self) -> None:
+        self.position=0
+        
